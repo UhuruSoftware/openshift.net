58d69a2491518fa4a84f0b1f0010ac2ce9b4176c
florindragos <florind@uhurusoftware.com>
2013-11-29 16:40:16 +0200

    Implemented Environ methods for reading contrainer env vars from disk
    Change-Id: Icccf84a32a38491713216119887dc26fdd2448a3

--- node/src/openshift-dotnet/Uhuru.Openshift.Node/Utils/Environ.cs
+++ node/src/openshift-dotnet/Uhuru.Openshift.Node/Utils/Environ.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using Uhuru.Openshift.Runtime.Config;
 
 namespace Uhuru.Openshift.Runtime.Utils
 {
@@ -9,8 +11,55 @@ namespace Uhuru.Openshift.Runtime.Utils
     {
         public static Dictionary<string, string> ForGear(string gearDir)
         {
-            // read env variables
-            return new Dictionary<string, string>();
+            return ForGear(gearDir, new string[] { });
+        }
+
+        public static Dictionary<string, string> ForGear(string gearDir, params string[] dirs)
+        {
+            Dictionary<string, string> env = Load(Path.Combine(NodeConfig.ConfigDir, "env"));
+            List<string> envDirs = new List<string>();
+            envDirs.Add(Path.Combine(gearDir, ".env"));            
+            foreach (string dir in Directory.GetDirectories(gearDir, "*"))
+            {
+                string envDir = Path.Combine(dir, "env");
+                if (Directory.Exists(envDir))
+                {
+                    envDirs.Add(envDir);
+                }
+            }
+            Load(envDirs.ToArray()).ToList().ForEach(x => env[x.Key] = x.Value);
+            foreach (string dir in Directory.GetDirectories(Path.Combine(gearDir, ".env"), "*"))
+            {
+                if (dir.EndsWith("user_vars"))
+                {
+                    continue;
+                }
+                Load(dir).ToList().ForEach(x => env[x.Key] = x.Value);
+            }
+            foreach(string dir in dirs)
+            {
+                Load(Path.Combine(dir, "env")).ToList().ForEach(x => env[x.Key] = x.Value);
+            }
+            Load(Path.Combine(gearDir, ".env", "user_vars")).ToList().ForEach(x => env[x.Key] = x.Value);
+            return env;
+        }
+
+        public static Dictionary<string, string> Load(params string[] dirs)
+        {            
+            Dictionary<string, string> env = new Dictionary<string, string>();
+            foreach (string dir in dirs)
+            {
+                string[] files = Directory.GetFiles(dir);
+                foreach (string file in files)
+                {
+                    if (file.EndsWith(".erb"))
+                    {
+                        continue;
+                    }
+                    env[Path.GetFileName(file)] = File.ReadAllText(file);
+                }
+            }
+            return env;
         }
     }
 }
