6c3078d388c1c86501da444df1d560b56094aaf0
adasescu <ada.sescu@uhurusoftware.com>
2013-12-19 16:17:48 +0000

    Added some SystemTests. (it assumes that RHC client is installed on windows node)
    Change-Id: I49523f0c531771b125b400c18336e51d9afa8ad2

---
+++ node/src/openshift-dotnet/Uhuru.OpenShift.SystemTests/TestHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uhuru.OpenShift.SystemTests
+{
+    public class TestHelper
+    {
+        public static string RunRHC(string command, bool setup)
+        {
+            string output = string.Empty;
+            using (Process p = new Process())
+            {
+                ProcessStartInfo info = new ProcessStartInfo(@"ruby");
+                info.Arguments = String.Format(@"{0}\rhc {1}", ConfigurationManager.AppSettings.Get("RubyPath"), command);
+                info.RedirectStandardInput = true;
+                info.RedirectStandardOutput = true;
+                info.RedirectStandardError = true;
+                info.UseShellExecute = false;
+                p.StartInfo = info;
+                p.Start();
+                // for running RHC setup, some interactions with the process are needed
+                if (setup && p != null)
+                {
+                    p.StandardInput.WriteLine("yes");
+                    p.StandardInput.Flush();
+                    p.StandardInput.WriteLine("\r\n");
+                    p.StandardInput.Flush();
+                    p.StandardInput.Close();
+                }
+                
+                output = p.StandardOutput.ReadToEnd();
+            }
+
+            return output;
+        }
+
+        public static bool SetupRHC()
+        {
+            string rhcConfig = Path.GetFullPath(ConfigurationManager.AppSettings.Get("RHCConfigFile"));
+
+            string output = TestHelper.RunRHC(String.Format("setup -l {0} -p {1} --create-token --config {2} ", ConfigurationManager.AppSettings.Get("RHCPassword"), ConfigurationManager.AppSettings.Get("RHCUsername"), rhcConfig), true);
+            
+            if (output.Contains("Your client tools are now configured."))
+            {
+                File.Copy(rhcConfig, String.Format("{0}\\.openshift\\express.conf", Environment.GetEnvironmentVariable("HOMEPATH")), true);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static string ProcessOuput(string output)
+        {
+
+            return "";
+        }
+    }
+}
---
+++ node/src/openshift-dotnet/Uhuru.OpenShift.SystemTests/Unit/AppManipulationTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Uhuru.OpenShift.SystemTests.Unit
+{
+
+    [TestClass]
+    public class AppManipulationTest
+    {
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void SetupRHC()
+        {
+            Assert.IsTrue(TestHelper.SetupRHC());
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void CreateDomain()
+        {
+            string domainName = "test2";
+
+            string output = TestHelper.RunRHC(String.Format("create-domain {0}", domainName), false);
+
+            Assert.IsTrue(output.Contains(String.Format("Creating domain '{0}' ... done", domainName)));
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void AppCreate()
+        {
+            
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void AppDeleteSucces()
+        {
+            string appName = "dotnet3";
+
+            //after the app was created, get app gear to check if the gear folder is deleted from window node location
+
+            string output = TestHelper.RunRHC(String.Format("app-delete {0} --confirm", appName), false);
+
+            Assert.IsTrue(output.Contains(String.Format("Deleting application '{0}' ... deleted", appName)));
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void AppDeleteFailed()
+        {
+            string appName = "dotnet3";
+
+            string output = TestHelper.RunRHC(String.Format("app-delete {0} --confirm", appName), false);
+
+            Assert.IsTrue(output.Contains(String.Format("Application '{0}' not found.", appName)));
+        }
+    }
+}
