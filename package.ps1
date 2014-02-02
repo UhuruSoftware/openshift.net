57484ff366a92839e71c5c0b7f83e0107fd6afa5
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2014-02-02 11:01:45 -0800

    Fixing the installation script so it restarts powershell in 64 bit mode if necessary.
    Change-Id: I2507083989a6eb2b54b41522936c97835eff06fd

--- package.ps1
+++ package.ps1
@@ -22,7 +22,7 @@ if (($pshome -like "*syswow64*") -and ((Get-WmiObject Win32_OperatingSystem).OSA
     write-warning "Restarting script under 64 bit powershell"
  
     # relaunch this script under 64 bit shell
-    & (join-path ($pshome -replace "syswow64", "sysnative")\powershell.exe) -file $myinvocation.mycommand.Definition @args
+    & (join-path ($pshome -replace "syswow64", "sysnative")\powershell.exe) -file  $SCRIPT:MyInvocation.MyCommand.Path @args
  
     # This will exit the original powershell process. This will only be done in case of an x86 process on a x64 OS.
     exit
