2b1c3b1ce8f9cd26e24ddf41049d0e49ca9fbb8d
Daniel Carabas <daniel.carabas@uhurusoftware.com>
2014-05-26 11:04:23 +0300

    Fix districts for mssql cartridge

--- node/src/openshift-dotnet/Uhuru.Openshift.Cmdlets/OO_Post_Configure.cs
+++ node/src/openshift-dotnet/Uhuru.Openshift.Cmdlets/OO_Post_Configure.cs
@@ -44,6 +44,9 @@ namespace Uhuru.Openshift.Cmdlets
         [Parameter]
         public string WithExposePorts;
 
+        [Parameter]
+        public int WithUid;
+
         protected override void ProcessRecord()
         {            
             this.WriteObject(Execute());
